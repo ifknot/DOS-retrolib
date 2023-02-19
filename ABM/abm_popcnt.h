104cc6cb87d997fb1d9eaa79c53de68a4e226f76
Dr. Jeremy Simon Thornton <ifknot@hotmail.com>
2023-02-19 01:26:18 +0000

    Wegner

--- ABM/abm_popcnt.h
+++ ABM/abm_popcnt.h
@@ -14,22 +14,24 @@
 namespace abm { // 8086 advanced bit manipulation 
 
     /**
-     * \brief Counts the number of 1 bits (population count) in a 64-bit unsigned integer.
-     * Wegner algorithm bitset cardinality algorithm optimised for the 8086 using fast paths for all 1s or as soon as all the 1s have been counted.
-     * As well as being ompetitive when the population count is relatively low (e.g., less than 4 one bit per 64-bit word) it offers fast path for the Wegner worst case all 1s.
-     * Further, it proves not only faster than a naive function checking the values of each bits but also
+     * \brief Uses the Wegner[1] algorithm to count the number of 1 bits (population count aka Hamming Weight) in a 64-bit unsigned integer.
+     * Wegner[1] algorithm bitset cardinality algorithm optimised for the 8086 using fast paths for all 1s or as soon as all the 1s have been counted.
+     * As well as being competitive when the population count is relatively low it offers a fast path for the Wegner worst case of all 1s.
+     * Further, it proves not only faster than a naive function checking the values of each bit (ROR or LSR) but also
      * + C implementation of the naive tree-of-adder approach and the 
      * + Wilkes-Wheeler-Gill function in C - due to the overhead of multiplication
+     * \note if 64K of system memory can be sacrificed then a Hamming Weight lookup table is faster.
+     * [1] Wegner, P. (1960) A technique for counting ones in a binary computer. Commun. ACM, 3, 322–.
+     * [2] https://en.wikipedia.org/wiki/Hamming_weight
      * \param ptr_value - the 64-bit unsigned integer for which we want the population count.
      * \return - the number of 1 bits in the quad word of memory pointed to.
      */
-    uint16_t popcnt64(uint64_t* ptr_value) {
-        uint16_t cnt;
+    uint16_t hamming_wegner_64(uint64_t* ptr_value) {
+        uint16_t hamming_weight;
         __asm {
             .8086
             sub     bx, bx              ; zero BX bit count
             lds     si, ptr_value       ; DS:SI points to 64bit quad word value
-            mov     cx, BITS_PER_WORD   ; set up the loop counter
             cld                         ; clear direction flag to increment DS:SI chain instructions
             
     WORD0:  lodsw                       ; load AX the first word of the qword value
@@ -37,51 +39,30 @@ namespace abm { // 8086 advanced bit manipulation
             jne     SHIFT0
             mov     bl, 16              ; it's all 1s
             jmp     WORD1               ; fast path the full word
-    SHIFT0: shr     ax, 1               ; shift lsb into the carry flag
-            jnc     NCF0                ; 0
-            inc     bl                  ; 1  increment bit count
-    NCF0:   jz      WORD1               ; no more set bits to count fast path to next word
-            loop    SHIFT0              ; count the remaining set bits
+    LOOP0   mov     cx, ax              ; Wegner algorithm
+            jcxz    WORD1               ; while not 0
+            dec     ax                  ; subtract 1
+            and     cx, ax              ; mask off
+    WORD1:
 
-    WORD1:  lodsw                       ; load AX the next word of the qword value
-            cmp     ax, 0xFFFF          
-            jne     SHIFT1
-            add     bl, 16              ; it's all 1s
-            jmp     WORD2               ; fast path the full word
-    SHIFT1: shr     ax, 1               ; shift lsb into the carry flag
-            jnc     NCF1                ; 0
-            inc     bl                  ; 1  increment bit count
-    NCF1:   jz      WORD2               ; no more set bits to count fast path to next word
-            loop    SHIFT1              ; count the remaining set bits
-
-    WORD2:  lodsw                       ; load AX the next word of the qword value
-            cmp     ax, 0xFFFF          
-            jne     SHIFT2
-            add     bl, 16              ; it's all 1s
-            jmp     WORD3               ; fast path the full word
-    SHIFT2: shr     ax, 1               ; shift lsb into the carry flag
-            jnc     NCF2                ; 0
-            inc     bl                  ; 1  increment bit count
-    NCF2:   jz      WORD3               ; no more set bits to count fast path to next word
-            loop    SHIFT2              ; count the remaining set bits
-
-    WORD3:  lodsw                       ; load AX the next word of the qword value
-            cmp     ax, 0xFFFF          
-            jne     SHIFT3
-            add     bl, 16              ; it's all 1s
-            jmp     END                 ; fast path the full word
-    SHIFT3: shr     ax, 1               ; shift lsb into the carry flag
-            jnc     NCF3                ; 0
-            inc     bl                  ; 1  increment bit count
-    NCF3:   jz      END                 ; no more set bits to count fast path to next word
-            loop    SHIFT3              ; count the remaining set bits
-
-    END:    mov     cnt, bx
+    END:    mov     hamming_weight, bx
 
         }
-        return cnt;
+        return hamming_weight;
     }
 
-}
+    /**
+     * \brief Uses a 64K lookup table to count the number of 1 bits (population count aka Hamming Weight) in a 64-bit unsigned integer.
+     * \url https://en.wikipedia.org/wiki/Hamming_weight
+     * \param ptr_value - the 64-bit unsigned integer for which we want the population count.
+     * \return - the number of 1 bits in the quad word of memory pointed to.
+     */
+    uint16_t hamming_lookup_64(uint64_t* ptr_value) {
+        uint16_t hamming_weight;
+        __asm {
+            .8086
+        }
+        return hamming_weight;
+    }
 
 #endif
--- ABM/test_abm.h
+++ ABM/test_abm.h
@@ -15,27 +15,27 @@ namespace test_abm {
 		{
 			INFO("test popcnt64");
 			uint64_t b = 0;
-			ASSERT(abm::popcnt64(&b) == 0, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 0, abm::hamming_wegner_64(&b));
 			b = 0xF;
-			ASSERT(abm::popcnt64(&b) == 4, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 4, abm::hamming_wegner_64(&b));
 			b = 0xFF;
-			ASSERT(abm::popcnt64(&b) == 8, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 8, abm::hamming_wegner_64(&b));
 			b = 0xFFFF;
-			ASSERT(abm::popcnt64(&b) == 16, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 16, abm::hamming_wegner_64(&b));
 			b = 0xFFFFFFFF;
-			ASSERT(abm::popcnt64(&b) == 32, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 32, abm::hamming_wegner_64(&b));
 			b = 0xFFFFFFFFFFFFFFFF;
-			ASSERT(abm::popcnt64(&b) == 64, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 64, abm::hamming_wegner_64(&b));
 			b = 0xA;
-			ASSERT(abm::popcnt64(&b) == 2, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 2, abm::hamming_wegner_64(&b));
 			b = 0xAA;
-			ASSERT(abm::popcnt64(&b) == 4, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 4, abm::hamming_wegner_64(&b));
 			b = 0xAAAA;
-			ASSERT(abm::popcnt64(&b) == 8, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 8, abm::hamming_wegner_64(&b));
 			b = 0xAAAAAAAA;
-			ASSERT(abm::popcnt64(&b) == 16, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 16, abm::hamming_wegner_64(&b));
 			b = 0xAAAAAAAAAAAAAAAA;
-			ASSERT(abm::popcnt64(&b) == 32, abm::popcnt64(&b));
+			ASSERT(abm::hamming_wegner_64(&b) == 32, abm::hamming_wegner_64(&b));
 			
 		}
 		return 0;
