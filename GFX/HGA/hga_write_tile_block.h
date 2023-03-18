33682870d2ddb1ee74799c02de7d981d30be3906
ifknot <ifknot@hotmail.com>
2023-03-18 23:45:11 +0000

    tile block

--- GFX/HGA/hga_write_tile_block.h
+++ GFX/HGA/hga_write_tile_block.h
@@ -69,51 +69,51 @@ TILE:       mov     ax, y                       ; load y into AX then perform sc
             cmp     ax, SCREEN_TILE_HEIGHT
             jge     END                         ; no more rows to plot
 
-            mov     di, dx                      ; load x into diand clip to screen bounds
+            mov     di, dx                      ; load x into DI and clip to screen bounds
             cmp     di, SCREEN_TILE_WIDTH
             jge     TEND                        ; no more columns on this row to plot
 
             shl     ax, 1                       ; convert y tile row to partial(x2) pixel location
             mov     cl, BYTES_PER_LINE
-            mul     cl                          ; calculate(y / 4) * 90 nb 133 cycles
-            add     di, ax                      ; +(y / 4) * 90
+            mul     cl                          ; calculate (y / 4) * 90 nb 133 cycles
+            add     di, ax                      ; + (y / 4) * 90
 
             lds     si, bytes                   ; DS:[SI] points to list of 8 tile data bytes to write
             add 	si, bx                      ; DS:[sI] points to the specific list of 8 tile bytes
             mov 	cx, w                       ; the width in *tiles* of the bitmap will step to correct column byte
             dec 	cx                          ; compensate for MOVSB auto increment
 
-            lodsb                               ; load 8 pixels strip tile row 0 into al
+            lodsb                               ; load 8 pixels strip tile row 0 into AL
             mov     es:[di] , al                ; store row 0 bank 0 as guaranteed bank zero start in tile space coords
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 1 into al
+            lodsb                               ; load 8 pixels strip tile row 1 into AL
             mov     es:[di + 2000h] , al        ; store row 1 bank 1
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 2 into al
+            lodsb                               ; load 8 pixels strip tile row 2 into AL
             mov     es:[di + 4000h] , al        ; store row 2 bank 2
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 3 into al
+            lodsb                               ; load 8 pixels strip tile row 3 into AL
             mov     es:[di + 6000h] , al        ; store row 3 bank 3
 
             add     di, 90                      ; add 90 bytes next line for all 4 banks
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 4 into al
+            lodsb                               ; load 8 pixels strip tile row 4 into AL
             mov     es:[di] , al                ; store row 4 bank 0
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 5 into al
+            lodsb                               ; load 8 pixels strip tile row 5 into AL
             mov     es:[di + 2000h] , al        ; store row 5 bank 1
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 6 into al
+            lodsb                               ; load 8 pixels strip tile row 6 into AL
             mov     es:[di + 4000h] , al        ; store row 6 bank 2
 
             add     si, cx
-            lodsb                               ; load 8 pixels strip tile row 7 into al
+            lodsb                               ; load 8 pixels strip tile row 7 into AL
             mov     es:[di + 6000h] , al        ; store row 7 bank 3
 
 TEND:       ret
