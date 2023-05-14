b36b5d3008fd2499edfc459de341bbc9895038f3
ifknot <ifknot@hotmail.com>
2023-05-14 15:33:51 +0100

    splash testing

--- GFX/HGA/hga_vram_word_write_bitmap.h
+++ GFX/HGA/hga_vram_word_write_bitmap.h
@@ -17,9 +17,14 @@
 
 namespace hga {
 
-
-	void vram_splash_bitmap(uint16_t y, uint16_t w, uint16_t h, const char* bytes, uint16_t buffer = GLOBAL::active_buffer) {
-		// TODO: screen clip bmp.height
+	/**
+	*  @brief fast writes screen width bitmap to VRAM assuming (0,y) coordinates
+	*  @param y      - vertical pixel screen postion
+	*  @param h      - bitmap height
+	*  @param bmp	 - a bitmap of 720 x h pixels i.e. HGA screen width
+	*  @param buffer - the VRAM buffer to write to
+	*/
+	void vram_splash_bitmap(uint16_t y, uint16_t h, const char* bytes, uint16_t buffer = GLOBAL::active_buffer) {
 		__asm {
 			.8086 
 			pushf
@@ -37,11 +42,7 @@ namespace hga {
             mul     cl							; calculate(y / 4) * 90 - nb 133 cycles
             mov     di, ax						; ES:[DI] = VRAM segment + (y / 4) * 90 
 
-			mov		dx, w						; load DX pixel width
-			shr		dx, 1						; DX = w / 8 to get number of bytes per bitmap row
-			shr		dx, 1						; 8086 single shifts only
-			shr		dx, 1						; ...
-			shr		dx, 1						; DX /= 2 to get them number of words per bitmap row 
+			mov		dx, WORDS_PER_LINE
 
 			mov		cx, h						; load CX with bmp height
 			jcxz	END
@@ -99,10 +100,7 @@ namespace hga {
 	*  @param bmp - a bitmap of 720 x 348 pixels i.e. HGA screen dimensions
 	*  @param buffer - the VRAM buffer to write to
 	*/
-	void vram_splash_bitmap(const gfx::simple_bitmap_t* bmp, uint16_t buffer = GLOBAL::active_buffer) {
-		assert(bmp->ihdr.width == SCREEN_X_MAX);
-		assert(bmp->ihdr.height == SCREEN_Y_MAX);
-		const char* bytes = bmp->idat.data;
+	void vram_splash_bitmap(const char* bytes, uint16_t buffer = GLOBAL::active_buffer) {
 		__asm {
 			.8086 
 			pushf
@@ -143,60 +141,7 @@ namespace hga {
 
 	namespace sync {
 
-		/**
-		*  @brief Waits for the vertival sync beforefast writes screen width and height bitmap to VRAM assuming (0,0) coordinates
-		*  @param bmp - a bitmap of 720 x 348 pixels i.e. HGA screen dimensions
-		*  @param buffer - the VRAM buffer to write to
-		*/
-		void vram_write_buffer(const gfx::simple_bitmap_t* bmp, uint16_t buffer = GLOBAL::active_buffer) {
-			//assert(bmp->ihdr.width == SCREEN_X_MAX);
-			//assert(bmp->ihdr.height == SCREEN_Y_MAX);
-			const char* bytes = bmp->idat.data;
-			__asm {
-				.8086
-				pushf
-				cld								; increment mode
-
-				mov     ax, HGA_VIDEO_RAM_SEGMENT
-				add     ax, buffer				; 0000h or 0B000h for first or second VRAM buffer
-				mov     es, ax					; ES points to screen segment
-				xor		di, di					; ES: [DI] points to top left of screen
-				lds     si, bytes				; DS: [SI] points to list of 8 tile data bytes to write
-
-				mov		cx, SCREEN_Y_MAX / 4
-
-				mov     dx, CRTC_STATUS_PORT	; set DX port 3BAh
-	VWAIT0:		in      al, dx					; read status
-				test    al, 10000000b			; is bit 7 clear ? (in a vertical retrace interval)
-				jz      VWAIT0					; yes, keep waiting
-	VWAIT1:		in      al, dx					; read status again
-				test    al, 10000000b			; is bit 7 clear ? (just started a vertical retrace interval)
-				jz      VWAIT1					; no, keep waiting
-
-	L0:			mov		bx, cx
-
-				mov     cx, WORDS_PER_LINE		; 45 words per line
-				rep     movsw					; copy a screen line to bank 0
-
-				add     di, 1FA6h				; DI and SI auto inc by 2 so bank 1 is DI + 2000h - 90
-				mov     cx, WORDS_PER_LINE		; 45 words per line
-				rep     movsw					; copy a screen line to bank 1
-
-				add     di, 1FA6h				; DI and SI auto inc by 2 so bank 2 is DI + 2000h - 90
-				mov     cx, WORDS_PER_LINE		; 45 words per line
-				rep     movsw					; copy a screen line to bank 2
-
-				add     di, 1FA6h				; DIand SI auto inc by 2 so bank 3 is DI + 2000h - 90
-				mov     cx, WORDS_PER_LINE		; 45 words per line
-				rep     movsw					; copy a screen line  to bank 0
-
-				sub		di, 6000h
-				mov		cx, bx
-				loop	L0
-
-				popf
-			}
-		}
+		
 
 	}
 
--- GFX/HGA/test_vram_buffer.h
+++ GFX/HGA/test_vram_buffer.h
@@ -36,15 +36,36 @@ namespace test_herc {
 			LOG(bios::detect_CRTC_at_port(bios::MDA_crtc_port));
 			LOG(hga::read_light_pen_registers());
 		}
+		gfx::simple_bitmap_t* bmp_splash = pbm::create_simple_bitmap("TEST/SPLASH.PBM");
 		{
-			INFO("\n1. test & time vram_write_buffer(bmp)\n");
-			gfx::simple_bitmap_t* bmp = pbm::create_simple_bitmap("TEST/SPLASH.PBM");	
+			INFO("\n1. test & time vram_splash_bitmap(0,0)\n");
+			if (YESNO("graphics mode? ")) {
+				hga::graphics_full_mode();
+				hga::cls();
+				hga::vram_splash_bitmap(bmp_splash->idat.data);
+				ANYKEY("");
+				hga::swap_buffers();
+
+				bios::set_system_clock_counter(0);
+				hga::cls();
+				for (int i = 0; i < n; ++i) {
+					hga::vsync();
+					hga::vram_splash_bitmap(bmp_splash->idat.data);
+				}
+				//ANYKEY("");
+				hga::text_half_mode();
+				std::cout << bios::read_system_clock_counter();
+			}
+		}
+		{
+			INFO("\n2. test & time vram_splash_bitmap(0,y)\n");
+			
 			uint16_t x = 0;
 			uint16_t y = 0;
 			if (YESNO("graphics mode? ")) {
 				hga::graphics_full_mode();
 				hga::cls();
-				hga::vram_splash_bitmap(y, bmp->ihdr.width, bmp->ihdr.height / 2, bmp->idat.data);
+				hga::vram_splash_bitmap(y, bmp_splash->ihdr.height / 2, bmp_splash->idat.data);
 				ANYKEY("");
 				hga::swap_buffers();
 				
@@ -52,17 +73,12 @@ namespace test_herc {
 				hga::cls();
 				for (int i = 0; i < n; ++i) {	
 					hga::vsync();
-					hga::vram_splash_bitmap(y, bmp->ihdr.width, bmp->ihdr.height / 2, bmp->idat.data);
+					hga::vram_splash_bitmap(y, bmp_splash->ihdr.height / 2, bmp_splash->idat.data);
 					y++;
-				}
-				
+				}				
 				//ANYKEY("");
 				hga::text_half_mode();
-				float t = 1;
 				std::cout << bios::read_system_clock_counter();
-				//std::cout << t << " ticks ";
-				//t = (t / n) / TICKS_PER_SECOND;
-				//std::cout << t << "Hz " << t << "fps\n";
 			}
 		}
 		return EXIT_SUCCESS;
