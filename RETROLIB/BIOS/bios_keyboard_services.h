a41e894c35d17abb886dad9228f2dc6f520f84a7
ifknot <ifknot@hotmail.com>
2023-08-09 15:00:40 +0100

    Update bios_keyboard_services.h

--- RETROLIB/BIOS/bios_keyboard_services.h
+++ RETROLIB/BIOS/bios_keyboard_services.h
@@ -4,7 +4,7 @@
  *  @details   ~
  *  @author    Jeremy Thornton
  *  @date      18.07.2022
- *  @copyright © Jeremy Thornton, 2022. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2022. All right reserved.
  *
  */
 #ifndef BIOS_KEYBOARD_SERVICES_H
@@ -12,32 +12,13 @@
 
 #include <stdint.h>
 
-#include "bios_keyboard_constants.h"
-#include "bios_keyboard_scan_codes.h"
+
 
 namespace bios {
 
-	uint8_t wait_key_ascii() {
-		uint8_t ascii = 0;
-		__asm {
-			.8086
-			mov		ah, WAIT_FOR_KEYSTROKE_AND_READ
-			int		BIOS_KEYBOARD_SERVICE
-			mov		ascii, al
-		}
-		return ascii;
-	}
+	uint8_t wait_key_ascii();
 
-	uint8_t wait_key_scan_code() {
-		uint8_t scan = 0;
-		__asm {
-			.8086
-			mov		ah, WAIT_FOR_KEYSTROKE_AND_READ
-			int		BIOS_KEYBOARD_SERVICE
-			mov		scan, ah
-		}
-		return scan;
-	}
+	uint8_t wait_key_scan_code();
 
 }
 
