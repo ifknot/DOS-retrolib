f8d8ae6e675ceb7438cf4b32820ab3154bc34702
ifknot <ifknot@hotmail.com>
2023-11-28 12:11:38 +0000

    Create gfx_bitmap.h

---
+++ RETROLIB/GFX/gfx_bitmap.h
@@ -0,0 +1,19 @@
+/****/
+#ifndef GFX_BITMAP_H
+#define GFX_BITMAP_H
+
+namespace gfx {
+
+  namespace bmp {
+
+  struct bitmap_t {
+
+  };
+
+  void load_pbm_bitmap(const char* file_path, bitmap_t* bmp);
+
+  }
+
+}
+
+#endif
