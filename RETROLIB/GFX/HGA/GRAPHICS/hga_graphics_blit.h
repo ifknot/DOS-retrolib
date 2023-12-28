713fb1277502534952ebcf6ae20a2800ff125565
ifknot <ifknot@hotmail.com>
2023-12-28 16:01:37 +0000

    Update hga_graphics_blit.h

--- RETROLIB/GFX/HGA/GRAPHICS/hga_graphics_blit.h
+++ RETROLIB/GFX/HGA/GRAPHICS/hga_graphics_blit.h
@@ -4,7 +4,7 @@
  *  @details   ~
  *  @author    Jeremy Thornton
  *  @date      19.12.2023
- *  @copyright © Jeremy Thornton, 2023. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2023. All right reserved.
  *
  */
 #ifndef HGA_GRPAHICS_BLIT_H
@@ -20,13 +20,17 @@ namespace hga {
 		* @brief fast raster bit block transfer 32768 bytes from memory to the 4 banks of a VRAM buffer 
 		* @note hard coded HGA screen rectangle (0,0,720,480)
 		*/
-		void blit(uint16_t vram_segment, char* raster_data);
+		void blit_vram_bmp(uint16_t vram_segment, char* raster_data);
 
 		/**
 		* @brief fast rectangular block (x,y,w,h) transfer raster bytes from memory to the same location in 4 banks of a VRAM buffer
 		* @note x and w values are rounded to the enclosing byte boundry 
 		*/
-		void blit(uint16_t vram_segment, char* raster_data, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+		void blit_vram_bmp(uint16_t vram_segment, char* raster_data, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+
+		void blit_bmp_bmp(char* raster_data1, char* raster_data2, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+
+		void blit_bmp_bmp(char* raster_data1, char* raster_data2, char* raster_mask, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
 
 	}
 
