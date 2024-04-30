c208f8b9b70804a9cce6aed592a8431e9fb8c81d
ifknot <ifknot@hotmail.com>
2024-04-30 14:34:53 +0100

    todo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #include "RETROLIB/test_retrolib.h"
 
+//TODO: 
+// 1. hga_sprite - takes a gfx_bitmap_t and calculates 8 offsets based on height and width (which must be a power of 2) for each x position pixel bitmap 0..7
+
 int main() {
 	
 	test_retrolib::run();
